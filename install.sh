e38d2875bf9cf18852691f8d5e36c55abfd24aca
Benjamin Cheng <ben@bcheng.cf>
2017-03-15 10:37:05 -0400

    Install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ln -s $DIR/vim $HOME/.vim
+ln -s $DIR/vimrc $HOME/.vimrc
+
+ln -s $DIR/Xresources $HOME/.Xresources
+xrdb $HOME/.Xresources
+
+ln -s $DIR/profile $HOME/.profile
+
+ln -s $DIR/termite $HOME/.config/termite
+
+ln -s $DIR/polybar $HOME/.config/polybar
+
+ln -s $DIR/i3 $HOME/.config/i3
