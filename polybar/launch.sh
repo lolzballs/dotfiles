acbdda992417ef7814df47d03e43a92e6de1d031
Benjamin Cheng <ben@bcheng.cf>
2017-04-20 16:36:27 -0400

    Use main bar on both monitors

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,5 +4,18 @@ killall -q polybar
 
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-polybar main &
-polybar secondary &
+nvidia=$(lsmod | grep -q nvidia)
+intern=eDP-1
+extern=HDMI-1
+
+if lsmod | grep -q "nvidia_drm"; then
+    intern="$intern-1"
+    extern="$extern-1"
+fi
+
+MONITOR=$intern polybar main &
+
+if xrandr | grep -q "$extern connected"
+then
+    MONITOR=$extern polybar main &
+fi
