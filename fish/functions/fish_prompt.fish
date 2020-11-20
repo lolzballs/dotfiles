e62b435c6d8ebc75675ee295cfbd7bc6c62b3572
Benjamin Cheng <ben@bcheng.me>
2020-11-20 11:37:47 -0500

    fish: fix ssh prompt spacing

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,13 +1,15 @@
 function fish_prompt
     if test -n "$SSH_TTY"
-        echo -n (set_color brred)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)' '
-    end
-    set_color -o
-    if test "$USER" = 'root'
-        echo -n (set_color red)'# '
+        echo -n (set_color brred)" $USER"(set_color white)'@'(set_color yellow)(prompt_hostname)
     end
 
-    echo -n (set_color blue)' ❯ '
+    set_color -o
+    if test "$USER" = 'root'
+        echo -n (set_color red)' # '
+    else
+        echo -n (set_color blue)' ❯ '
+    end
+
     echo -n (set_color blue)(prompt_pwd)' '
 
     set_color normal
